babffe28b7344f626e9c9b8448db6e8c573950d6
vadim_manakow <vadim_manakow@mail.ru>
2025-12-11 02:00:04 +0300

    bugfix: перенес Providers

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -1,5 +1,5 @@
 import * as mockData from '../mock-server/data';
-import { ReactQueryProvider } from './ReactQueryProvider';
+import { ReactQueryProvider } from '../providers/ReactQueryProvider';
 import * as returnOrder from './return-order';
 import * as products from './products';
 import * as returnOrders from './return-orders';
